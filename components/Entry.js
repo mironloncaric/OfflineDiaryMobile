2c95bd0807bada18b3a0525a7c5a02ce4f76644a
Miron Lončarić <miron.loncaric@gmail.com>
2020-11-11 21:27:55 +0100

    Created the body for the Entry component

--- components/Entry.js
+++ components/Entry.js
@@ -5,44 +5,59 @@ import React from 'react'
 export default function Entry(props) {
     return (
         <View style={{
-            backgroundColor:'#dbdbdb',
-            borderRadius:2,
-            padding:7,
-            width:'100%',
-            flexDirection:'row',
-            flexWrap:'wrap'
+            borderColor:'#c9c9c9',
+            borderWidth:1
         }}>
-            <Text style={{
-                width:'40%'
-            }}>Dear Diary,</Text>
             <View style={{
-                width:'60%',
+                backgroundColor:'#dbdbdb',
+                borderRightWidth:1,
+                borderRightColor:'#c9c9c9',
+                borderRadius:2,
+                padding:7,
+                width:'100%',
+                flexDirection:'row',
+                flexWrap:'wrap'
             }}>
+                <Text style={{
+                    width:'40%'
+                }}>Dear Diary,</Text>
                 <View style={{
-                    flexDirection:'row',
-                    flexWrap:'wrap'
+                    width:'60%',
                 }}>
-                    <Text style={{
-                        width:'70%',
-                        textAlign:'right',
-                        paddingRight:10
-                    }}>7/7/2000 16:16</Text>
-                    <Text style={{
-                        width:'20%',
-                        textAlign:'center',
-                        borderRightWidth: 1,
-                        borderRightColor: '#9c9c9c',
-                        borderLeftWidth: 1,
-                        borderLeftColor: '#9c9c9c'
-                    }}>😀</Text>
-                    <TouchableHighlight style={{
-                        width:'7%',
+                    <View style={{
+                        flexDirection:'row',
+                        flexWrap:'wrap'
                     }}>
                         <Text style={{
-                            textAlign:'right'
-                        }}>x</Text>
-                    </TouchableHighlight>
+                            width:'70%',
+                            textAlign:'right',
+                            paddingRight:10
+                        }}>7/7/2000 16:16</Text>
+                        <Text style={{
+                            width:'20%',
+                            textAlign:'center',
+                            borderRightWidth: 1,
+                            borderRightColor: '#9c9c9c',
+                            borderLeftWidth: 1,
+                            borderLeftColor: '#9c9c9c'
+                        }}>😀</Text>
+                        <TouchableHighlight style={{
+                                width:'10%',
+                            }}
+                            onPress={ () => console.log('Hello world') }
+                        >
+                            <Text style={{
+                                textAlign:'left',
+                                paddingLeft:10
+                            }}>x</Text>
+                        </TouchableHighlight>
+                    </View>
                 </View>
+            </View>
+            <View style={{
+                padding:5
+            }}>
+                <Text>Incididunt occaecat duis est magna proident quis ut ex Lorem enim dolore ut.</Text>
             </View>
         </View>
     )
